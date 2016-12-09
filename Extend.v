73bec534e84033025c639cb9a621971acdef24f2
Jianfei2333 <xjf999999@hotmail.com>
2016-12-09 12:54:57 +0800

    Create Extend.v

---
+++ Extend.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module Extend(
+	input [15:0] immediate,
+	input ExtSel,
+	output [31:0] extended_immediate
+    );
+
+	assign extended_immediate = (ExtSel)?{{16{immediate[15]}}, immediate[15:0]}
+											:{{16{1'b0}}, immediate[15:0]};
+endmodule
